9d821f263f68210d5333479c2ebaa7360494b408
Mozhganshafiee27 <mozhganshafiee27@gmail.com>
2022-01-26 10:00:24 +0330

    try to get dependent objects based on departments

--- test/src/components/Slider.js
+++ test/src/components/Slider.js
@@ -3,8 +3,7 @@ import Carousel from "react-material-ui-carousel";
 import { Paper, Button, Grid, Box, Modal } from "@mui/material";
 
 function Slider() {
-  const [artWorks, setArtworks] = useState([]);
-  const [departments, setDepartments] = useState([]);
+  const [testDepartments, setTestDepartments] = useState([]);
   const [detail, setDetail] = useState([]);
   const [open, setOpen] = useState(false);
 
@@ -17,53 +16,79 @@ function Slider() {
     setOpen(false);
     setDetail([]);
   };
-  
-  
-
-  //get number of objects which has details and set it in a state
-  const makeCollection = async () => {
-    let artWork = [];
-    for (let i = 100; i < 110; i++) {
-      const responses = await fetch(
-        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${i}`
-      );
-      await responses.json().then((data) => {
-        // console.log(data);
-        artWork.push(data);
-      });
-    }
-    setArtworks(artWork);
-  };
 
   //fetch apis while page loading
-  useEffect(() => {
-    //fetch depertments
-    fetch(
+  useEffect(async () => {
+    let info = [];
+    let dep = {};
+    let departmentsData = [];
+
+    //fetch all depertments
+    const response = await fetch(
       "https://collectionapi.metmuseum.org/public/collection/v1/departments"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setDepartments(data.departments);
+    );
+    await response.json().then((data) => {
+      console.log(data.departments);
+      data.departments.map(async (department) => {
+        //fetch all objects depend on every depertment
+        const responses = await fetch(
+          `https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${department.departmentId}`
+        );
+        await responses.json().then(async (data) => {
+          console.log(data);
+          let ids;
+          //pick limited number of objects based on departments
+          for (let i = 1; i < 5; i++) {
+            ids = data.objectIDs[i];
+            //fetch more details about objects
+            const responsess = await fetch(
+              `https://collectionapi.metmuseum.org/public/collection/v1/objects/${ids}`
+            );
+            await responsess.json().then((data) => {
+              info.push(data);
+            });
+          }
+          // data.objectIDs.map(async (id) => {
+          //   const responsess = await fetch(
+          //     `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`
+          //   );
+          //   await responsess.json().then((data) => {
+          //     info.push(data);
+          //   });
+          // });
+
+          //create new object consist of it's depertment title, some object id depend on department, object detail
+          dep = {
+            id: data.objectIDs,
+            title: department.displayName,
+            details: info,
+          };
+          // console.log(dep);
+          // departmentsData.push(dep);
+          // departmentsData = [...departmentsData, dep];
+        });
       });
-
-    makeCollection();
+    });
+    //create new state of all deps for map in ui
+    setTestDepartments(departmentsData);
   }, []);
-
-  // console.log(artWorks);
+  // console.log(testDepartments);
 
   //make each slide for 4 arts
   let items = [];
-  for (let i = 0; i < artWorks.length; i += 4) {
+  for (let i = 0; i < 11; i += 4) {
     items.push(
       <Grid container spacing={0} className="cardLayout" key={i}>
-        {artWorks.slice(i, i + 4).map((item, i) => {
+        {testDepartments.slice(i, i + 4).map((dep, i) => {
           return (
             <div className="card">
               <Paper key={i.toString()}>
-                <img src={item.primaryImageSmall} />
-                <h3>{item.objectID}</h3>
+                <img src={dep.details.primaryImageSmall} />
+                <h3>{dep.id}</h3>
               </Paper>
-              <Button className="modalButton" onClick={() => handleOpen(item)}>{item.title}</Button>
+              <Button className="modalButton" onClick={() => handleOpen(dep)}>
+                {dep.title}
+              </Button>
             </div>
           );
         })}
@@ -72,10 +97,10 @@ function Slider() {
   }
   return (
     <React.Fragment>
-      {departments.map((department) => {
+      {testDepartments.map((department) => {
         return (
-          <div key={department.departmentId}>
-            <div className="title">{department.displayName}</div>
+          <div key={department.id}>
+            <div className="title">{department.title}</div>
             <Carousel autoPlay={false} navButtonsAlwaysVisible>
               {items}
             </Carousel>
